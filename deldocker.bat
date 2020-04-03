3117789786c04081fb0a8658775004db64731fb7
jesse_back <ihlee@jlk-inspection.com>
2020-04-03 15:08:39 +0900

    my-app

---
+++ deldocker.bat
@@ -0,0 +1,4 @@
+docker stop django-sample-for-docker-compose_db_1
+docker rm django-sample-for-docker-compose_db_1
+docker rm django-sample-for-docker-compose_django_1
+docker rmi -f django-sample-for-docker-compose_django
